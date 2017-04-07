03d949897a1da99b4340d915f23342ba72204403
Aslam <hunzaboy@gmail.com>
2017-04-07 16:38:13 +0500

    updated js file

--- dist/js/ckin.js
+++ dist/js/ckin.js
@@ -5,7 +5,7 @@
    git+https://github.com/hunzaboy/ckin.git
 */
 // Source: https://gist.github.com/k-gun/c2ea7c49edf7b757fe9561ba37cb19ca;
-(function () {
+(function() {
     // helpers
     var regExp = function regExp(name) {
         return new RegExp('(^| )' + name + '( |$)');
@@ -23,14 +23,14 @@
 
     ClassList.prototype = {
         add: function add() {
-            forEach(arguments, function (name) {
+            forEach(arguments, function(name) {
                 if (!this.contains(name)) {
                     this.element.className += ' ' + name;
                 }
             }, this);
         },
         remove: function remove() {
-            forEach(arguments, function (name) {
+            forEach(arguments, function(name) {
                 this.element.className = this.element.className.replace(regExp(name), '');
             }, this);
         },
@@ -60,7 +60,7 @@
         DOMTokenList.prototype.replace = ClassList.prototype.replace;
     }
 })();
-(function () {
+(function() {
     if (typeof NodeList.prototype.forEach === "function") return false;
     NodeList.prototype.forEach = Array.prototype.forEach;
 })();
@@ -83,35 +83,35 @@ function browserSniff() {
     }
     // MSIE
     else if ((verOffset = nAgt.indexOf("MSIE")) !== -1) {
-            browserName = "IE";
-            fullVersion = nAgt.substring(verOffset + 5);
+        browserName = "IE";
+        fullVersion = nAgt.substring(verOffset + 5);
+    }
+    // Chrome
+    else if ((verOffset = nAgt.indexOf("Chrome")) !== -1) {
+        browserName = "Chrome";
+        fullVersion = nAgt.substring(verOffset + 7);
+    }
+    // Safari
+    else if ((verOffset = nAgt.indexOf("Safari")) !== -1) {
+        browserName = "Safari";
+        fullVersion = nAgt.substring(verOffset + 7);
+        if ((verOffset = nAgt.indexOf("Version")) !== -1) {
+            fullVersion = nAgt.substring(verOffset + 8);
         }
-        // Chrome
-        else if ((verOffset = nAgt.indexOf("Chrome")) !== -1) {
-                browserName = "Chrome";
-                fullVersion = nAgt.substring(verOffset + 7);
-            }
-            // Safari
-            else if ((verOffset = nAgt.indexOf("Safari")) !== -1) {
-                    browserName = "Safari";
-                    fullVersion = nAgt.substring(verOffset + 7);
-                    if ((verOffset = nAgt.indexOf("Version")) !== -1) {
-                        fullVersion = nAgt.substring(verOffset + 8);
-                    }
-                }
-                // Firefox
-                else if ((verOffset = nAgt.indexOf("Firefox")) !== -1) {
-                        browserName = "Firefox";
-                        fullVersion = nAgt.substring(verOffset + 8);
-                    }
-                    // In most other browsers, "name/version" is at the end of userAgent
-                    else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) < (verOffset = nAgt.lastIndexOf('/'))) {
-                            browserName = nAgt.substring(nameOffset, verOffset);
-                            fullVersion = nAgt.substring(verOffset + 1);
-                            if (browserName.toLowerCase() == browserName.toUpperCase()) {
-                                browserName = navigator.appName;
-                            }
-                        }
+    }
+    // Firefox
+    else if ((verOffset = nAgt.indexOf("Firefox")) !== -1) {
+        browserName = "Firefox";
+        fullVersion = nAgt.substring(verOffset + 8);
+    }
+    // In most other browsers, "name/version" is at the end of userAgent
+    else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) < (verOffset = nAgt.lastIndexOf('/'))) {
+        browserName = nAgt.substring(nameOffset, verOffset);
+        fullVersion = nAgt.substring(verOffset + 1);
+        if (browserName.toLowerCase() == browserName.toUpperCase()) {
+            browserName = navigator.appName;
+        }
+    }
     // Trim the fullVersion string at semicolon/space if present
     if ((ix = fullVersion.indexOf(";")) !== -1) {
         fullVersion = fullVersion.substring(0, ix);
@@ -146,14 +146,7 @@ var iconVolumeLow = '<i class="ckin-volume-low"></i>';
 var iconExpand = '<i class="ckin-expand"></i>';
 var iconCompress = '<i class="ckin-compress"></i>';
 
-// var iconPlay = '<i class="typcn typcn-media-play"></i>';
-// var iconPause = '<i class="typcn typcn-media-pause"></i>';
-// var iconVolumeMute = '<i class="typcn typcn-volume-mute"></i>';
-// var iconVolumeMedium = '<i class="typcn typcn-volume-up"></i>';
-// var iconVolumeLow = '<i class="typcn typcn-volume-down"></i>';
-
-
-players.forEach(function (player) {
+players.forEach(function(player) {
     var video = player.querySelector('video');
 
     var skin = attachSkin(video.dataset.ckin);
@@ -187,46 +180,46 @@ players.forEach(function (player) {
         playerControls.style.display = "none";
     }
 
-    video.addEventListener('click', function () {
+    video.addEventListener('click', function() {
         togglePlay(this, player);
     });
-    video.addEventListener('play', function () {
+    video.addEventListener('play', function() {
         updateButton(this, toggle);
     });
 
-    video.addEventListener('pause', function () {
+    video.addEventListener('pause', function() {
         updateButton(this, toggle);
     });
-    video.addEventListener('timeupdate', function () {
+    video.addEventListener('timeupdate', function() {
         handleProgress(this, progressBar);
     });
 
-    toggle.forEach(function (button) {
-        return button.addEventListener('click', function () {
+    toggle.forEach(function(button) {
+        return button.addEventListener('click', function() {
             togglePlay(video, player);
         });
     });
-    volumeButton.addEventListener('click', function () {
+    volumeButton.addEventListener('click', function() {
         toggleVolume(video, volumeButton);
     });
 
     var mousedown = false;
-    progress.addEventListener('click', function (e) {
+    progress.addEventListener('click', function(e) {
         scrub(e, video, progress);
     });
-    progress.addEventListener('mousemove', function (e) {
+    progress.addEventListener('mousemove', function(e) {
         return mousedown && scrub(e, video, progress);
     });
-    progress.addEventListener('mousedown', function () {
+    progress.addEventListener('mousedown', function() {
         return mousedown = true;
     });
-    progress.addEventListener('mouseup', function () {
+    progress.addEventListener('mouseup', function() {
         return mousedown = false;
     });
-    fullScreenButton.addEventListener('click', function (e) {
+    fullScreenButton.addEventListener('click', function(e) {
         return toggleFullScreen(player, fullScreenButton);
     });
-    addListenerMulti(player, 'webkitfullscreenchange mozfullscreenchange fullscreenchange MSFullscreenChange', function (e) {
+    addListenerMulti(player, 'webkitfullscreenchange mozfullscreenchange fullscreenchange MSFullscreenChange', function(e) {
         return onFullScreen(e, player);
     });
 });
@@ -235,7 +228,7 @@ function showControls(video) {
 
     video.setAttribute("controls", "controls");
 }
-/* Build out functions */
+
 function togglePlay(video, player) {
     var method = video.paused ? 'play' : 'pause';
     video[method]();
@@ -244,7 +237,7 @@ function togglePlay(video, player) {
 
 function updateButton(video, toggle) {
     var icon = video.paused ? iconPlay : iconPause;
-    toggle.forEach(function (button) {
+    toggle.forEach(function(button) {
         return button.innerHTML = icon;
     });
 }
@@ -254,16 +247,8 @@ function skip() {
 }
 
 function toggleVolume(video, volumeButton) {
-    // var level = video.volume ? '0' : '0.5';
     var level = video.volume;
     var icon = iconVolumeMedium;
-
-    // if (level == 1) {
-    //     level = 0;
-    // } else {
-    //     level = level + .25;
-    // }
-
     if (level == 1) {
         level = 0;
         icon = iconVolumeMute;
@@ -291,27 +276,23 @@ function scrub(e, video, progress) {
     var scrubTime = e.offsetX / progress.offsetWidth * video.duration;
     video.currentTime = scrubTime;
 }
-// Build the default HTML
+
 function wrapPlayers() {
 
     var videos = document.querySelectorAll('video');
 
-    videos.forEach(function (video) {
+    videos.forEach(function(video) {
 
-        // create wrapper container
         var wrapper = document.createElement('div');
         wrapper.classList.add('ckin__player');
 
-        // insert wrapper before video in the DOM tree
         video.parentNode.insertBefore(wrapper, video);
 
-        // move el into wrapper
         wrapper.appendChild(video);
     });
 }
 
 function buildControls(skin) {
-    // Create html array
     var html = [];
     html.push('<button class="' + skin + '__button--big toggle" title="Toggle Play">' + iconPlay + '</button>');
 
@@ -356,16 +337,14 @@ function addColor(player, color) {
         var buttons = player.querySelectorAll('button');
         var progress = player.querySelector('.progress__filled');
         progress.style.background = color;
-        buttons.forEach(function (button) {
+        buttons.forEach(function(button) {
             return button.style.color = color;
         });
     }
 }
 
 function toggleFullScreen(player, fullScreenButton) {
-    // let isFullscreen = false;
-    if (!document.fullscreenElement && // alternative standard method
-    !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+    if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
         player.classList.add('ckin__fullscreen');
 
         if (player.requestFullscreen) {
@@ -380,8 +359,6 @@ function toggleFullScreen(player, fullScreenButton) {
         isFullscreen = true;
 
         fullScreenButton.innerHTML = iconCompress;
-        // fullScreenButton.classList.remove('icon-fullscreen-alt');
-        // fullScreenButton.classList.add('icon-fullscreen-exit-alt');
     } else {
         player.classList.remove('ckin__fullscreen');
 
@@ -406,11 +383,6 @@ function onFullScreen(e, player) {
     }
 }
 
-/* Add one or more listeners to an element
- ** @param {DOMElement} element - DOM element to add listeners to
- ** @param {string} eventNames - space separated list of event names, e.g. 'click change'
- ** @param {Function} listener - function to attach for each event as a listener
- */
 function addListenerMulti(element, eventNames, listener) {
     var events = eventNames.split(' ');
     for (var i = 0, iLen = events.length; i < iLen; i++) {
